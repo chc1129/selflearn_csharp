807b20c835128cc6ecdc843c0be7898256b7b97d
chc1129 <chc1129@gmail.com>
2022-03-24 06:39:54 +0900

    cs prg fix

---
+++ Chap03/NameOfBasic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SelfCSharp.Chap03
+{
+    class NameOfBasic
+    {
+        int data = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(nameof(NameOfBasic)); // 結果:NameOfBasic
+            Console.WriteLine(nameof(data)); // 結果:data
+        }
+    }
+}
---
+++ Chap03/NameOfNull.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SelfCSharp.Chap03
+{
+    class NameOfNull
+    {
+        public static void Hoge(string str)
+        {
+            if (str == null)
+            {
+                throw new ArgumentNullException("str");
+                // throw new ArgumentNullException(nameof(str);
+            }
+            Console.WriteLine(str);
+        }
+
+        static void Main(string[] args)
+        {
+            var nc = new NameOfNull();
+            nc.Hoge(null);
+        }
+    }
+}
