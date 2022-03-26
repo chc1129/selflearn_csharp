5dd255ace5459c839557c037d589657edbef9af1
chc1129 <chc1129@gmail.com>
2022-03-27 07:56:40 +0900

    cs prg fix

---
+++ Chap04/SwitchType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SelfCSharp.Chap04
+{
+    class SwitchType
+    {
+        static void Main(string[] args)
+        {
+            object obj = -123;
+            // object obj = "wings";
+            switch (obj)
+            {
+                case int i:
+                    Console.WriteLine(Math.Abs(i));
+                    break;
+                case string str:
+                    Console.WriteLine(str[0]);
+                    break;
+                default:
+                    Console.WriteLine("意図しない型です。");
+                    break;
+            }
+        }
+    }
+}
