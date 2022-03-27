769a88a978d662a5b2bf5e01a2d2f8af138b75b1
chc1129 <chc1129@gmail.com>
2022-03-27 11:06:09 +0900

    cs prg fix

---
+++ Chap04/WhileOmit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SelfCSharp.Chap04
+{
+    class WhileOmit
+    {
+        static void Main(string[] args)
+        {
+            var i = 1;
+            while (i < 6)
+                Console.WriteLine(i++);
+                Console.WriteLine("--------");
+        }
+    }
+}
